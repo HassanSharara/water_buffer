6c62c66aabe25da8199f24cdda8c461ac9205046
HassanSharara <hassansharara12@gmail.com>
2025-12-11 23:09:36 +0300

    support circular buffer

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
 
     let mut buf = water_buffer::WaterBuffer::with_capacity(12);
     buf.extend_from_slice(b"hello world");
-    println!("{:?}",String::from_utf8_lossy(&buf[..]));
+    println!("{:?}",String::from_utf8_lossy(&buf));
 
 }
