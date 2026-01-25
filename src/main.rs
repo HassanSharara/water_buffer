ae67d1ee4ff1f8742303d4b32c0b8dc92b5898eb
HassanSharara <hassansharara12@gmail.com>
2026-01-25 05:39:56 +0300

    e

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
-use bytes::Buf;
-// use bytes::{Buf, BytesMut};
-use water_buffer::WaterBuffer;
 
 
 fn main(){
 
-    let mut b = bytes::BytesMut::with_capacity(10);
-    b.extend_from_slice(b"12");
-    b.advance(1);
-    println!("called {}",b.capacity());
-    return;
-    // unsafe {dos();}
-    // std::alloc::Global;
-    let mut b = WaterBuffer::with_capacity(20);
-    b.extend_from_slice(b"Hello, world!");
-    b.extend_from_slice(b"2222222222222");
-    println!("{:?} {}",String::from_utf8_lossy(&b),b.capacity());
-    b.advance(13);
-    println!("{:?} {}",String::from_utf8_lossy(&b),b.capacity());
-    b.extend_from_slice(b"3333333333333");
-    println!("{:?} {}",String::from_utf8_lossy(&b),b.capacity());
-    b.extend_from_slice(b"4444444444444");
-    println!("{:?} {}",String::from_utf8_lossy(&b),b.capacity());
+
 }
