100740c1ff092fdb347429b0f5b9c04bc5074b15
HassanSharara <hassansharara12@gmail.com>
2026-01-31 22:57:50 +0300

    adding support for tokio uring

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use water_buffer::WaterBuffer;
 
 fn main(){
 
-    let buf = WaterBuffer::with_capacity(200);
-    {
-        let mut b2 = unsafe {buf.unsafe_clone()};
-        b2.extend_from_slice(b"hello world");
-        println!("{:?} {:?}",buf,b2);
-    }
-
-    println!("buf {:?}",String::from_utf8_lossy(&buf[..]));
+    // let buf = WaterBuffer::with_capacity(200);
+    // {
+    //     let mut b2 = unsafe {buf.unsafe_clone()};
+    //     b2.extend_from_slice(b"hello world");
+    //     println!("{:?} {:?}",buf,b2);
+    // }
+    //
+    // println!("buf {:?}",String::from_utf8_lossy(&buf[..]));
 }
